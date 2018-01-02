775853257704ac68f4c31bab6833c576f703d744
Steph <stephmilani@umbc.edu>
2018-01-02 16:44:04 -0500

    Update RockSample.java

--- src/rocksample/RockSample.java
+++ src/rocksample/RockSample.java
@@ -1,28 +1,14 @@
 package rocksample;
 
-import burlap.behavior.singleagent.Episode;
-import burlap.behavior.singleagent.auxiliary.EpisodeSequenceVisualizer;
-import burlap.behavior.singleagent.learning.tdmethods.QLearning;
 import burlap.mdp.auxiliary.DomainGenerator;
 import burlap.mdp.core.TerminalFunction;
 import burlap.mdp.core.action.UniversalActionType;
-import burlap.mdp.core.oo.ObjectParameterizedAction;
-import burlap.mdp.core.state.State;
-import burlap.mdp.singleagent.environment.SimulatedEnvironment;
 import burlap.mdp.singleagent.model.FactoredModel;
 import burlap.mdp.singleagent.model.RewardFunction;
 import burlap.mdp.singleagent.oo.OOSADomain;
-import burlap.mdp.singleagent.oo.ObjectParameterizedActionType;
-import burlap.statehashing.HashableStateFactory;
-import burlap.statehashing.simple.SimpleHashableStateFactory;
 import rocksample.state.RoverAgent;
 import rocksample.state.RockSampleWall;
 import rocksample.state.RockSampleRock;
-import rocksample.stateGenerator.RockSampleObservationStateFactory;
-import rocksample.stateGenerator.RockSampleStateFactory;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by steph on 10/26/2017.
@@ -31,51 +17,51 @@ import java.util.List;
 public class RockSample implements DomainGenerator {
 
     // object classes
-    public static final String CLASS_ROVER = "Rover";
-    public static final String CLASS_ROCK = "Rock";
-    public static final String CLASS_WALL = "Wall";
+    public static final String CLASS_ROVER = 				"Rover";
+    public static final String CLASS_ROCK =                 "Rock";
+    public static final String CLASS_WALL =                 "Wall";
 
     // abstract class
-    public static final String CLASS_PT = "Point";
+    public static final String CLASS_PT =                   "Point";
 
     // common attributes
-    public static final String ATT_X = "x";
-    public static final String ATT_Y = "y";
+    public static final String ATT_X =                      "x";
+    public static final String ATT_Y =                      "y";
 
     // rock attributes
-    public static final String ATT_QUALITY = "quality";
+    public static final String ATT_QUALITY =                "quality";
 
     // wall attributes
-    public static final String ATT_START_X = "startX";
-    public static final String ATT_START_Y = "startY";
-    public static final String ATT_LENGTH = "length";
-    public static final String ATT_IS_HORIZONTAL = "isHorizontal";
+    public static final String ATT_START_X = 				"startX";
+    public static final String ATT_START_Y = 				"startY";
+    public static final String ATT_LENGTH = 				"length";
+    public static final String ATT_IS_HORIZONTAL =			"isHorizontal";
 
     // colors
-    public static final String COLOR_RED = "red";
-    public static final String COLOR_YELLOW = "yellow";
-    public static final String COLOR_GREEN = "green";
-    public static final String COLOR_BLUE = "blue";
-    public static final String COLOR_MAGENTA = "magenta";
-    public static final String COLOR_BLACK = "black";
-    public static final String COLOR_GRAY = "gray";
+    public static final String COLOR_RED = 					"red";
+    public static final String COLOR_YELLOW = 				"yellow";
+    public static final String COLOR_GREEN = 				"green";
+    public static final String COLOR_BLUE = 				"blue";
+    public static final String COLOR_MAGENTA =				"magenta";
+    public static final String COLOR_BLACK = 				"black";
+    public static final String COLOR_GRAY =					"gray";
 
     // actions
-    public static final int NUM_MOVE_ACTIONS = 4;
-    public static final String ACTION_NORTH = "north";
-    public static final String ACTION_EAST = "east";
-    public static final String ACTION_SOUTH = "south";
-    public static final String ACTION_WEST = "west";
-    public static final String ACTION_SAMPLE = "sample";
-    public static final String ACTION_CHECK = "check";
+    public static final int NUM_MOVE_ACTIONS = 				4;
+    public static final String ACTION_NORTH = 				"north";
+    public static final String ACTION_EAST =				"east";
+    public static final String ACTION_SOUTH =				"south";
+    public static final String ACTION_WEST = 				"west";
+    public static final String ACTION_SAMPLE =              "sample";
+    public static final String ACTION_CHECK =               "check";
 
     // action indexes
-    public static int IND_NORTH = 0;
-    public static int IND_EAST = 1;
-    public static int IND_SOUTH = 2;
-    public static int IND_WEST = 3;
-    public static int IND_SAMPLE = 4;
-    public static int IND_CHECK = 5;
+    public static int IND_NORTH = 							0;
+    public static int IND_EAST = 							1;
+    public static int IND_SOUTH = 							2;
+    public static int IND_WEST = 							3;
+    public static int IND_SAMPLE =                          4;
+    public static int IND_CHECK =                           5;
 
     // parameters dictating probabilities of the model
     private RewardFunction rf;
@@ -87,13 +73,11 @@ public class RockSample implements DomainGenerator {
 
     //observations
     public String[] observations;
-
     /**
      * RockSample
      * A RockSample domain generator
-     *
-     * @param r               reward function
-     * @param t               terminal function
+     * @param r reward function
+     * @param t terminal function
      * @param correctMoveProb probability that the rover will move in selected
      *                        direction
      * @param
@@ -124,22 +108,20 @@ public class RockSample implements DomainGenerator {
         this.tf = new RockSampleTerminalFunction();
     }
 
-    public RockSample() {
-        this(1, false, 0);
-    }
+    public RockSample() { this(1, false, 0); }
 
     private void setMoveDynamics(double correctProb) {
         moveDynamics = new double[NUM_MOVE_ACTIONS][NUM_MOVE_ACTIONS];
 
-        for (int choose = 0; choose < NUM_MOVE_ACTIONS; choose++) {
-            for (int outcome = 0; outcome < NUM_MOVE_ACTIONS; outcome++) {
-                if (choose == outcome) {
+        for(int choose = 0; choose < NUM_MOVE_ACTIONS; choose++){
+            for(int outcome = 0; outcome < NUM_MOVE_ACTIONS; outcome++){
+                if(choose == outcome){
                     moveDynamics[choose][outcome] = correctProb;
                 }
                 // the two directions which are one away get the rest of prob
-                else if (Math.abs(choose - outcome) % 2 == 1) {
+                else if(Math.abs(choose - outcome) % 2 == 1){
                     moveDynamics[choose][outcome] = (1 - correctProb) / 2;
-                } else {
+                }else{
                     moveDynamics[choose][outcome] = 0;
                 }
             }
@@ -170,13 +152,14 @@ public class RockSample implements DomainGenerator {
         return domain;
     }
 
-  /** public static void main(String[] args) {
+
+   /* public static void main(String[] args) {
         RockSample rocksampleBuild = new RockSample();
         OOSADomain domain = rocksampleBuild.generateDomain();
 
         HashableStateFactory hs = new SimpleHashableStateFactory();
 
-        State s = RockSampleObservationStateFactory.createClassicState();
+        State s = RockSampleStateFactory.createClassicState();
         SimulatedEnvironment env = new SimulatedEnvironment(domain, s);
 
         List<Episode> eps = new ArrayList<Episode>();
@@ -193,5 +176,5 @@ public class RockSample implements DomainGenerator {
                 domain, eps);
         v.setDefaultCloseOperation(v.EXIT_ON_CLOSE);
         v.initGUI();
-    } **/
+    }*/
 }
